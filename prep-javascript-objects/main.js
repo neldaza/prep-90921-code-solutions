5f33318c6dc5865fe81958bc70a55a9cda542934
Nelson David Daza <yankeenelson@gmail.com>
2021-09-22 11:24:02 -0400

    Prep Javascript Objects First Draft

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,19 @@
+var person = {
+  firstName: 'John',
+  lastName: 'Smith',
+  hobby: 'baseball',
+  job: 'plumber',
+  previousJob: 'teacher'
+};
+console.log(person);
+var fullName = 'John ' + 'Smith';
+var nameDeclare = "The person's full name is";
+console.log(nameDeclare, fullName);
+var currentJob = 'plumber';
+var currentJobDeclare = "The person's current job is";
+console.log(currentJobDeclare, currentJob);
+var previousJob = 'teacher';
+var previousJobDeclare = "The person's previous job was";
+console.log(previousJobDeclare, previousJob);
+var completePerson = 'The compelte person object:';
+console.log(completePerson, person);
